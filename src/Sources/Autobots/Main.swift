76a9765c84aad97a5abd38f98d65576cf456347d
Nikita Gorbunov <gorbunovna@bcsmarkets>
2023-06-26 18:00:48 +0300

    промежуточный коммит

---
+++ src/Sources/Autobots/Main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Autobots
+//
+//  Created by Nikita Gorbunov on 26.06.2023.
+//
+
+import Foundation
+
+@main
+public struct Main {
+    public static func main() {
+        print("-----")
+        print(ProcessInfo.processInfo.environment)
+        print("----")
+    }
+}
+
