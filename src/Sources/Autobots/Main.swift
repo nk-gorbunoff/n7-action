af4de66339145109a58c3a0ede38636ad08efb38
Nikita Gorbunov <gorbunovna@bcsmarkets>
2023-06-26 18:27:04 +0300

    промежуточный коммит

--- src/Sources/Autobots/Main.swift
+++ src/Sources/Autobots/Main.swift
@@ -10,9 +10,42 @@ import Foundation
 @main
 public struct Main {
     public static func main() {
-        print("-----")
-        print(ProcessInfo.processInfo.environment)
-        print("----")
+        
+        guard let slackToken = ProcessInfo.processInfo.environment["INPUT_SLACK_BOT_AUTH_TOKEN"],
+              let channelID = ProcessInfo.processInfo.environment["INPUT_SLACK_CHANNEL_ID"] else {
+            print("ERROR ERROR")
+            return
+        }
+        
+        let message = "Hello, Slack!"
+        API().postMessageToSlack(token: slackToken, channel: channelID, message: message)
     }
 }
 
+
+struct API {
+    func postMessageToSlack(token: String, channel: String, message: String) {
+        let url = URL(string: "https://slack.com/api/chat.postMessage")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let parameters = [
+            "token": token,
+            "channel": channel,
+            "text": message
+        ]
+        
+        let jsonData = try! JSONSerialization.data(withJSONObject: parameters)
+        
+        let task = URLSession.shared.uploadTask(with: request, from: jsonData) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+            } else if let data = data,
+                      let dataString = String(data: data, encoding: .utf8) {
+                print("Response: \(dataString)")
+            }
+        }
+        
+        task.resume()
+    }
+}
